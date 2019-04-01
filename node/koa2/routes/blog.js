9889bb3205f8fcd19a8d6d6e36a3b15c48e4a237
赵安东 <zhaoandong4213@sina.com>
2019-04-01 20:26:59 +0800

    04/01

---
+++ node/koa2/routes/blog.js
@@ -0,0 +1,17 @@
+const router = require('koa-router')()
+
+router.prefix('/api/blog')
+
+router.get('/list', async function (ctx, next) {
+  const query = ctx.query
+  ctx.body = {
+    errno: 0,
+    query,
+    data: ['数据']
+  }
+})
+
+module.exports = router
+
+
+
